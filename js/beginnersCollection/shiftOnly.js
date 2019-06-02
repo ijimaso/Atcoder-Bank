6d3177426253138ad7d6c8478b9848ceaa5ee6cf
ijimaso <soxup0708@gmail.com>
2019-06-03 04:04:15 +0900

    first commit

---
+++ js/beginnersCollection/shiftOnly.js
@@ -0,0 +1,27 @@
+const shift = (N, numbers, sum = 0) => {
+    const newNumbers = [];
+    for (let number of numbers) {
+        number = parseInt(number, 10);
+        if (number % 2 !== 0) {
+            console.log(sum);
+            break;
+        } else {
+            number /= 2;
+            newNumbers.push(number);
+        }
+    }
+    if (newNumbers.length === N) {
+        sum += 1;
+        shift(N, newNumbers, sum);
+    }
+};
+
+const main = (input) => {
+    input = input.split("\n");
+    const N = parseInt(input[0], 10);
+    const numbers = input[1].split(" ");
+    shift(N, numbers, 0);
+};
+
+main(require("fs").readFileSync("input.txt", "utf8"));
+// main(require("fs").readFileSync("/dev/stdin", "utf8"));
---
+++ js/beginnersCollection/testInput.js
@@ -0,0 +1,14 @@
+const main = (input) => {
+    console.log(input);
+    input = input.split("\n");
+    const tmp = input[1].split(" ");
+    console.log(input);
+    console.log(tmp);
+    const a = parseInt(input[0], 10);
+    const b = parseInt(tmp[0], 10);
+    const c = parseInt(tmp[1], 10);
+    const s = input[2];
+    console.log(`${a + b + c} ${s}`);
+};
+
+main(require("fs").readFileSync("input.txt", "utf8"));
