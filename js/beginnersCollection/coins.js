1c0a2a7b19aeb677ce78e1358dd12ac5671ccf1a
ijimaso <soxup0708@gmail.com>
2019-06-03 05:27:23 +0900

    update

--- js/beginnersCollection/coins.js
+++ js/beginnersCollection/coins.js
@@ -1,11 +1,11 @@
 const main = (input) => {
-    input = input.split("\r\n");
+    input = input.split("\n");
     const A = Number(input[0]);
     const B = Number(input[1]);
     const C = Number(input[2]);
     const X = Number(input[3]);
     let order = 0;
-    
+
     for (let a = 0; a <= A; a++) {
         for (let b = 0; b <= B; b++) {
             for (let c = 0; c <= C; c++) {
